fe56e118c08d863254cfb4db6ecf4e1cef32b39a
bschmalb <bastian.schmalbach@smail.th-koeln.de>
2020-04-05 00:21:49 +0200

    2 new html pages, adding and removing tipps, rest api updates and expanded

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var app = require('express')();
+
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const fs = require('fs');
 var cors = require('cors');
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended : true}));
+const tipps = require("/home/pi/Documents/htmlServer/data/tipps.json");
+
+app.use(express.json({
+  type: ['application/json', 'text/plain']
+}))
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(cors());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("X-Content-Type-Options", "bastianschmalbach.de");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -21,26 +30,50 @@ app.get('/addTipps', function (req, res) {
   res.sendFile('/home/pi/Documents/htmlServer/web-app/addTipps.html');
 });
 
+app.get('/editTipps', function (req, res) {
+  res.sendFile('/home/pi/Documents/htmlServer/web-app/editTipps.html');
+});
+
 app.get('/tipps', function (req, res) {
-  fs.readFile('data/tipps.json', (err, data) => {
-    if (err) throw err;
-    let tipps = JSON.parse(data);
-    res.status(200).send(JSON.stringify(tipps))
-  });
+  res.status(200).send(tipps);
+});
+
+app.get('/tipps/:id', function (req, res) {
+  var tipp = {};
+  tipps.forEach(element => {
+    if (element.id === req.params.id) tipp = element;
+  })
+  if (tipp.id != undefined) res.status(200).send(tipp);
+  else res.status(404).send("Tipp does not exist");
 });
 
 app.post('/tipps', function (req, res) {
-    console.log(req.body);
-    
-    fs.readFile('data/tipps.json', (err, data) => {
-      if (err) throw err;
-      let tipps = JSON.parse(data);
-      //console.log(tipps);
-    });
-    
-    //tipps.push(tipp);
+  tipps.unshift(req.body);
+  fs.writeFile("/home/pi/Documents/htmlServer/data/tipps.json", JSON.stringify(tipps), err => {
+    if (err) throw err;
+    console.log("Done writing"); // Success 
+  });
+  res.send('Post erfolgreich');
+});
 
-    res.send('Der Tipp wurde erfolgreich hinzugefügt');
+app.delete('/tipps/:id', function (req, res) {
+  for (var i = 0; i < tipps.length; i++) {
+    if (tipps[i].id === req.params.id) {
+      console.log(tipps[i]);
+      tipps.splice(i, 1);
+      fs.writeFile("/home/pi/Documents/htmlServer/data/tipps.json", JSON.stringify(tipps), err => {
+        if (err) throw err;
+        console.log("Done writing"); // Success 
+      });
+    }
+  }
+  res.status(200).send(tipps);
+
+  /* tipps.splice(req.body.tippNumber, 1)
+  fs.writeFile("/home/pi/Documents/htmlServer/data/tipps.json", JSON.stringify(tipps), err => {
+    if (err) throw err;
+    console.log("Done writing");
+  });*/
 });
 
 app.get('/challenges', function (req, res) {
@@ -50,14 +83,6 @@ app.get('/challenges', function (req, res) {
     res.status(200).send(JSON.stringify(challenges))
   });
 });
-
-function getTipps() {
-  fs.readFile('data/tipps.json', (err, data) => {
-    if (err) throw err;
-    let tipps = JSON.parse(data);
-    return tipps;
-  });
-}
 
 io.on('connection', (client) => {
   console.log('A client connected\t', client.id);
