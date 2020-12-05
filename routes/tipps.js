6e972aad36b74b9fa049d99fa834616f31fc62e8
bschmalb <bastian.schmalbach@smail.th-koeln.de>
2020-12-05 12:47:16 +0100

    server request architecture change

--- routes/tipps.js
+++ routes/tipps.js
@@ -11,8 +11,8 @@ const ReportedTipp = require('../models/ReportedTipp')
 router.get('/', async function (req, res) {
     try {
         if (req.body.minscore != null) {
-            const tipps = await Tipp.find(req.query).sort({ "$natural": -1 });
-            tipps.filter(tipp => tipp.score > req.body.minscore);
+            const tipps2 = await Tipp.find(req.query).sort({ "$natural": -1 });
+            const tipps = tipps2.filter(tipp => tipp.score > req.body.minscore);
             res.status(200).json(tipps)
         } else if (req.body.maxscore != null) {
             const tipps = await Tipp.find(req.query).sort({ "$natural": -1 });
